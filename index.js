933f18fa4187cd7b1ed2ad7a1e9ca999d62c9c41
ruzanna07 <khachatryanruzanna5@gmail.com>
2022-05-08 05:57:20 -0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const http = require("http");
+const HOSTNAME = "127.0.0.1";
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.end("hello world");
+});
+
+server.listen(PORT, HOSTNAME, () => {
+    console.log(`Server running at http://${HOSTNAME}:${PORT}`);
+});
